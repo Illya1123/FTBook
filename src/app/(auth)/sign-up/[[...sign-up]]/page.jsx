c03a664f5d59b96a2b88f66292bdd999bfeeacfc
Illya1123 <21520565@gm.uit.edu.vn>
2024-04-12 13:32:33 +0700

    create footer and design page sign up & sign in

--- src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,12 @@
-import { SignUp } from "@clerk/nextjs";
- 
+import { SignUp } from '@clerk/nextjs';
+
 export default function Page() {
-  return <SignUp />;
+	return (
+		<div className='mx-auto flex max-w-[1200px] items-center justify-between py-4'>
+      <div className='mx-auto flex text-center'>
+        <p className='p-4 text-4xl font-bold text-red-500'>Hãy đăng ký để trở thành thành viên của FT BOOK</p>
+      </div>
+			<div><SignUp /></div>
+		</div>
+	);
 }
